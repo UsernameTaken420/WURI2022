89213e3fb4954eb8952a03d3451b39419ca753c4
UsernameTaken420 <Nico_Gabin@yahoo.com>
2022-05-28 20:22:12 -0300

    Add request
    It now makes a GET request at the URL used with the command

--- wuri.py
+++ wuri.py
@@ -1,2 +1,19 @@
 import sys
-import requests-html
+import requests
+from requests.exceptions import HTTPError
+
+if __name__ == "__main__":
+    try:
+        url = "https://" + sys.argv[1]
+        response = requests.get(url)
+        if (response.status_code == 200):
+            response.encoding = 'utf-8'
+            print(response.content)
+    except HTTPError as http_err:
+        print("HTTP Error ocurred: " + http_err)
+    except IndexError as index:
+        print("No URL")
+    except Exception as err:
+        print("Error: " + err)
+
+        # TODO: parse HTML and grep "rss"
